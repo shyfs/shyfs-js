5dd738e8eac94d5a51461ed3a583956f6b063bbd
Math W <mathewsto51@gmail.com>
2021-09-20 06:54:56 -0300

    feat: sla

---
+++ tests/client.test.ts
@@ -0,0 +1,15 @@
+import { ShyClient } from '../src'
+
+const ShyClientFactory = () => {
+  return new ShyClient(
+    'http://localhost:8888/api/organizations/shy-fs-tester-17bffe8688f',
+    '$argon2id$v=19$t=3,m=4096,p=1$cM5dPH7SJJac4A9qqH39Ug$TNUAVOTXtHJf1aWxjb6+5uvbMEwWdK+xsFlzN7Ivaos'
+  )
+}
+
+describe('shy client', () => {
+  it('should works', async () => {
+    const client = ShyClientFactory()
+    expect(client.url).toBe(client.url)
+  })
+})
